e7a4b01b724ccbde5bdeb72dc1397768f888ef8c
justin <justin>
2026-05-15 17:52:40 +0800

    api-implementation-for-student

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -3,13 +3,9 @@ package core
 import (
 	"fmt"
 	"log"
-	"net"
-	"net/http"
 	models "smeag_sms_be/internal/models/config"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
-	"github.com/soheilhy/cmux"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
@@ -28,30 +24,9 @@ func NewServerInstance(h *models.HostConfig, r *gin.Engine, cdb *mongo.Database)
 }
 
 func (s *ServerInstance) EstablishedServer() {
+	address := fmt.Sprintf(":%s", s.host.Port)
 
-	// convert port
-	port, err := strconv.Atoi(s.host.Port)
-	if err != nil {
-		log.Fatalf("port err:%v", err)
+	if err := s.router.Run(address); err != nil {
+		log.Fatalf("failed to start server: %v", err)
 	}
-
-	address := fmt.Sprintf(":%d", port)
-	listener, err := net.Listen("tcp", address)
-
-	if err != nil {
-		log.Fatalf("cannot start server :%v", err)
-	}
-
-	m := cmux.New(listener)
-	httpL := m.Match(cmux.HTTP1Fast())
-
-	// Serve Http/1 and handle gin router engine
-	httpServer := &http.Server{Handler: s.router}
-	go httpServer.Serve(httpL)
-
-	// Serve mutliplexing
-	if err := m.Serve(); err != nil {
-		log.Fatalf("unable to serve :%v", err)
-	}
-
 }
